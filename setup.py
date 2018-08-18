1220970251a879f14e58afa3dd5e37b6c485ecea
Daniel K <code.danielk@gmail.com>
2018-08-18 02:49:21 +0200

    setup.py: add long_description

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
 
+import os
 from setuptools import setup
+
+
+def readfile(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname), 'r') as f:
+        return f.read()
 
 
 setup(
     name='trampoline',
-    version='0.1.0',
+    version='0.1.1',
     description='Simple and tiny yield-based trampoline implementation.',
+    long_description=readfile('README.rst'),
     license='MIT',
     author_email='code.danielk@gmail.com',
     url='https://gitlab.com/ferreum/trampoline',
